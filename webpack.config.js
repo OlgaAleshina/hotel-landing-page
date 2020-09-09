2fe1be0a74d9956c568dcde3e0589446fcdbd9dc
Olga <ovg43@yandex.ru>
2020-09-09 20:48:19 +0300

    add fonts locally

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,18 @@ const config = {
       {
         test: /\.(s*)css$/,
         use: [miniCss.loader, "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/"
+            }
+          }
+        ]
       }
     ]
   }
